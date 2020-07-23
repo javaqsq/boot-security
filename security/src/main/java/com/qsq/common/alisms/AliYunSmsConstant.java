8cba2aee73327db297ebf2c98c3177cfa68ab59e
qsq <1113948520@qq.comgit>
2020-07-23 20:47:32 +0800

    修改私密数据

--- security/src/main/java/com/qsq/common/alisms/AliYunSmsConstant.java
+++ security/src/main/java/com/qsq/common/alisms/AliYunSmsConstant.java
@@ -11,7 +11,7 @@ public class AliYunSmsConstant {
     /**
      * 您的验证码${code}，该验证码5分钟内有效，请勿泄漏于他人！
      */
-    public static final String SMS_SIMPLE_TEMPLATE = "SMS_164195079";
+    public static final String SMS_SIMPLE_TEMPLATE = "youTemplate";
 
     /**
      * 短信签名
